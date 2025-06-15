266760241b48220a55b88f4551b549c6396de052
gian <gianbeko99@gmail.com>
2025-06-15 08:12:40 +0800

    update init dan compose

--- init.sql
+++ init.sql
@@ -2,14 +2,14 @@ CREATE DATABASE IF NOT EXISTS db_akademik;
 
 USE db_akademik;
 
-CREATE TABLE IF NOT EXISTS matakuliah (
+CREATE TABLE IF NOT EXISTS mata_kuliah (
     id INT AUTO_INCREMENT PRIMARY KEY,
     kode_mk VARCHAR(10) NOT NULL UNIQUE,
     nama_mk VARCHAR(50) NOT NULL,
     sks INT NOT NULL
 );
 
-INSERT INTO matakuliah (kode_mk, nama_mk, sks) VALUES
+INSERT INTO mata_kuliah (kode_mk, nama_mk, sks) VALUES
 ('DO188', 'Cloud Computing', 3),
 ('RH294', 'Pemrograman Sistem Jaringan', 3)
 ON DUPLICATE KEY UPDATE kode_mk=kode_mk;
